c2fcdc0f114e10a2207e970d9e104e012552659b
mesut <semihturgutlu@gmail.com>
2022-02-04 11:01:57 +0300

    little change is done

--- Js Array Methods/some.js
+++ Js Array Methods/some.js
@@ -37,18 +37,25 @@
 
 //The following example shows how to check if any number in the marks array is in the range of (8, 10
 
-let marks = [3,56,7,9,5,8,6,45,4];
+// let marks = [3,56,7,9,5,8,6,45,4];
 
-const range = {
-    min:1,
-    max:2,
-};
+// const range = {
+//     min:1,
+//     max:2,
+// };
 
-let result = marks.some(function (e){
-    return e >=this.min && e <=this.max;
-}, range);
+// let result = marks.some(function (e){
+//     return e >=this.min && e <=this.max;
+// }, range);
 
+// console.log(result);
+
+
+
+//If you call the some() method on an empty array, the result is always false regardless of any condition. For example:
+
+let result = [].some(e => e>0);
 console.log(result);
 
-
-
+result = [].some(e => e<10);
+console.log(result);
