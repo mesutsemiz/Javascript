e64dbf0ab9c923c9d1b61a19a47f6269ebb03088
mesut <semihturgutlu@gmail.com>
2022-02-05 11:34:58 +0300

    updated

---
+++ js_useful_things/ternary.js
@@ -0,0 +1,10 @@
+
+//Ternary syntax 
+// condition ? expressionIfTrue :expressionIfFalse;
+
+let age = 18;
+let message;
+
+age >= 16 ? (message = 'You can drive.') : (message = 'You cannot drive.');
+
+console.log(message);
