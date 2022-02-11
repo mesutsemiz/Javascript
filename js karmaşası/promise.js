6c2e25ddd7f1cf78cff8546c46a160f45739d5d5
mesut <semihturgutlu@gmail.com>
2022-02-11 16:37:23 +0300

    updated

--- js karmaşası/promise.js
+++ js karmaşası/promise.js
@@ -61,3 +61,39 @@ learnJS.catch(
 
 3-- finally() method sometimes, you want to execute the same piece of code whether the promise is fulfilled or rejected.
 */
+
+// let p = new Promise((resolve,reject)=>{
+
+//     let a=1+1
+//     if (a ==2){
+//         resolve("Success")
+//     }else{
+//         reject("Failed")
+//     }
+
+// });
+
+// p.then((message)=>{
+//     console.log("this is the in the the " + message)
+// }).catch((error)=>{
+//     console.log("this is inthe catch "+ error)
+// })
+
+console.log("start");
+
+function loginUser(email,passwords, callback) {
+    setTimeout(() => {
+        console.log("Nw we have data..")
+        callback(email, passwords)
+        
+    }, 5000);
+}
+
+const user = loginUser("devedf@gmail.com", 12345678, (user)=>{
+    console.log(user)
+});
+
+
+
+
+console.log("Finish")
