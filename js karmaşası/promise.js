2a39d0f4fed821f65cc2f050151c24386c4082ce
mesut <semihturgutlu@gmail.com>
2022-02-12 15:35:21 +0300

    updated

--- js karmaşası/promise.js
+++ js karmaşası/promise.js
@@ -230,3 +230,68 @@ learnJS.catch(
 //     })
 // })
 
+
+
+
+
+
+//////////////////////PROMİSE chaning syntax
+
+// step1()
+//     .then(result =>step2(result))
+//     .then(result =>step3(result))
+
+
+//     //////////this code is same below code
+
+// step1()
+//     .then(step2)
+//     .then(step3)
+
+
+
+/******************Suppose that you want to perform the following asynchronous operations in sequence:
+
+    >>>>>>>First, get the user from the database.
+    >>>>>>>Second, get the services of the selected user.
+    >>>>>>>Third, calculate the service cost from the user’s services.
+
+The following functions illustrate the three asynchronous operations: */
+
+function getUser(userId) {
+    return new Promise((resolve, reject) =>{
+        console.log("Get the user from datatbase")
+        setTimeout(() => {
+            resolve({ 
+                userId:userId,
+                username:"admin"
+            })
+        }, 5000);
+    })
+}
+
+function getServices(user) {
+    return new Promise((resolve, reject)=>{
+        console.log(`get services of ${user.username} from the API`)
+        setTimeout(() => {
+            resolve(["Email", "VPN", "CDN"])
+            
+        }, 3000);
+
+    })
+}
+
+function getServiceCost(services){
+    return new Promise((resolve, reject) => {
+        console.log(`calculate th service cost of ${services}.`)
+        setTimeout(() => {
+            resolve(services.length*100);
+            
+        }, 3000);
+    })
+}
+
+getUser(100)
+.then(getServices)
+.then(getServiceCost)
+.then(console.log);
