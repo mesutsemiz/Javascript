030e3798fa6dc2454fcd1e5185fde41604d390f1
mesut <semihturgutlu@gmail.com>
2022-02-16 13:02:19 +0300

    updated

---
+++ js_useful_things/function.js
@@ -0,0 +1,49 @@
+// let sayi= "time"
+
+// function say(message) {
+    
+//     console.log(message)
+// }
+
+// say(sayi[3])
+
+
+// showMe();
+
+// function showMe() {
+//     console.log("an hoisting is working...")
+// }
+
+
+// var state="time"
+// function returnState() {
+    
+//     return state
+// }
+// console.log(returnState())
+
+
+// let show = (function() {
+//     console.log('Anonymous function');
+// });
+
+// show();
+
+// (function () {
+//     console.log("immediately invoked functions");
+
+// })()
+
+// let person = {
+//     firstName : " Mesut",
+//     lastName : " Semiz",
+// };
+
+// (function () {
+//     console.log("My name is"+ person.firstName + person.lastName)
+// })(person);
+
+NOTE;
+//Anonymous functions are functions witout name
+//Anonyous functions can be used as an arguments to other functions or as an immediately
+//invoked function execution.
