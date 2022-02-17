a332a06704dff839969e555a263004f273b24c15
mesut <semihturgutlu@gmail.com>
2022-02-17 15:47:01 +0300

    splice(): JS Array method

---
+++ string methods/splice().js
@@ -0,0 +1,18 @@
+// const score =[1,3,5,6,4,8]
+
+// const newScore = score.splice(0,3)
+
+// console.log(newScore)
+
+// console.log(score)
+
+// let languages = ['C', 'C++', 'Java', 'JavaScript'];
+
+// languages.splice(1, 3, 'Python');
+
+// console.log(languages); 
+
+// let myFinish =["angel", "clown", "mandarin", "sturgeo"]
+// let removed =myFinish.splice(2, 2, "drum")
+// console.log(removed) /// delete items stored new variable !!!!!!
+// console.log(myFinish)
